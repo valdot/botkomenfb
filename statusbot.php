ae026378f9fe4e6f9527533b550de31e5c0a2775
valdot <45897344+valdot@users.noreply.github.com>
2018-12-16 15:27:13 +0700

    Create statusbot.php

---
+++ statusbot.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Note : Edit/Ganti Kalimat  Hello World & Morning
+Dengan Kalimat Yang Mau Kamu Gunakan Menjadi Status !!!
+example : "Aku Sayang Kamu ","Aku Cinta Kamu","Dan seterusnya Jika Mau Lebih Banyak" */
+$status = array (
+  "Hello World","Morning"
+);
+?>
